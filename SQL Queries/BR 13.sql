3d60a1e619c9a0bf89d18497542e181e318b781a
AIFAZ SHAH <aifaz.shah246@gmail.com>
2026-03-05 11:12:24 +0530

    Create BR 13.sql

---
+++ SQL Queries/BR 13.sql
@@ -0,0 +1,28 @@
+## Business Requirement
+** Calculate the cumulative sales per month for each year.**
+
+
+	SELECT 
+    YEAR(o.order_purchase_timestamp) AS Year,
+    MONTH(o.order_purchase_timestamp) AS Month,
+
+    -- Monthly sales
+    SUM(p.payment_value) AS Monthly_Sales,
+
+    -- Cumulative sales
+    SUM(SUM(p.payment_value)) OVER
+    (
+        PARTITION BY YEAR(o.order_purchase_timestamp)
+        ORDER BY MONTH(o.order_purchase_timestamp)
+    ) AS Cumulative_Sales
+
+FROM dbo.orders$ o
+
+JOIN dbo.payments$ p
+ON o.order_id = p.order_id
+
+GROUP BY 
+    YEAR(o.order_purchase_timestamp),
+    MONTH(o.order_purchase_timestamp)
+
+ORDER BY Year, Month;
